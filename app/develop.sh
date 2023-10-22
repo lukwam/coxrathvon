2738496e49420e2ba7314a01044d218f9186a996
Lukas Karlsson <karlsson@altissimo.io>
2023-10-22 14:10:07 -0400

    Commit current version.

--- app/develop.sh
+++ app/develop.sh
@@ -10,7 +10,7 @@ docker run -it --rm \
     -e GOOGLE_CLOUD_PROJECT="${PROJECT_ID}" \
     -e GOOGLE_PROJECT_ID="${PROJECT_ID}" \
     -e PORT=8080 \
-    -p 8081:8080 \
+    -p 8080:8080 \
     -v "$(pwd):/workspace" \
     -v "$(pwd)/tmp:/tmp" \
     -v "$(pwd)/etc:/secrets" \
